123449921ff4a98fd42aeef99e26bb3b5f6598c7
Andy Cavedal <hcavedal@gmail.com>
2023-07-13 11:35:44 -0300

    exporting for esteban

---
+++ routes/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/global.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <h1>Hello World!</h1>
+    )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+		mode: 'development',
+    entry: './src/index.js',
+    //donde vivira el projecto =>
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        //nombre del enpaquetado que se va a crear
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    //que extensiones se van a usar
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    //Aqui trabajaremos las reglas que se crearan con los loaders y los plugins que agreguemos
+    module: {
+        rules: [
+            //configuraciones necesarias dentro de objetos
+            {
+                //aqui le decimos con que tiene que trabajar
+                test: /\.(js|jsx)$/,
+                //aqui le decimos que no queremos que lea en este proyecto
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+
+                ]
+            },
+            {
+                //le decimos que trabaje con html
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    //nos permite agregar plugins que queremos usar
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+    }
+}
