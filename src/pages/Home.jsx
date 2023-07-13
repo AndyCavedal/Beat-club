66caf85a61fa4a08ceb0a3ee1493467764b32395
3steban99 <barrionuevo99@outlook.com>
2023-07-13 19:29:54 -0300

    ejemplo de banner en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Home.scss';
 import neon from '../assets/logoneon.svg';
+import beat from '../assets/beat-club-logo-header.png'
+import { Link } from "react-router-dom";
 
 
 const Home = () =>{
@@ -8,8 +10,18 @@ const Home = () =>{
     
     return(
         <div className='background'>
-            <img src={neon} alt="s" className='neon'/>
-            <h1>Hola Esteban</h1>
+            <div className='contenedor-banner'>
+                <div>
+                    <img src={beat} alt="a" />
+                </div>
+                <p>Sabores y música en vivo en un solo lugar.</p>
+                <p>¡Te esperamos!</p>
+                <hr></hr>
+                <div className='banner-btn'>
+                    <Link to='/contacto' className='btn'>Contactanos</Link>
+                </div>
+                <p className='banner-direccion'>Av. Perú 1775, M5500 Mendoza</p>
+            </div>
         </div>
     )
 }
