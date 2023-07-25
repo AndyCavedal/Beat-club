d5b0aeb7894a1c1dee75bba21c12aa599949a3c0
3steban99 <barrionuevo99@outlook.com>
2023-07-25 15:58:40 -0300

    prueba de menu con apirest con peticion get

--- src/components/MenuTable.jsx
+++ src/components/MenuTable.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import '../styles/MenuTable.scss';
-import vinos from '../assets/menu/vinos-carta.jpg';
+import imgVinos from '../assets/menu/vinos-carta.jpg';
+import imgEspumante from '../assets/menu/espumantes.jpg'
 
 
 const MenuTable = () => {
 
-    // Datos de ejemplo para el menú (nombre y precio)
-    const menuItems = [
-        { name: 'Paradigma - Domaine ST Diego', price: "3,700.00" },
-        { name: 'Pura Sangre - Domaine ST Diego', price: "4,200.00" },
-        { name: 'Pura Sangre 9 Lunas - Domaine ST Diego', price: "8,000.00" },
-        { name: 'Casa Boher - Gran Reserva', price: "6,100.00" },
-        { name: 'Casa Boher - Gran Corte9400', price: "9,400.00" },
-        { name: 'Cabernet Frank-Santa Bax (Orgnanico)', price: "3,000.00" },
-        { name: 'Septima Obra - Tardio 500ml', price: "2,000.00" },
-        { name: 'Copa de Vino 150ml', price: "600.00" },
-        // Agrega más elementos según tus necesidades
-    ];
+    const [vinos, setVinos] = useState([])
+    const [espumante, setEspumante] = useState([])
+
+    useEffect(()=>{
+        getVino()
+        getEspumante()
+    },[])
+
+    function getVino(){
+        axios.get("https://server-api-beat-club.vercel.app/productos/vino").then((resp)=>{
+            setVinos(resp.data);
+        })
+    }
+
+    function getEspumante() {
+        axios.get("https://server-api-beat-club.vercel.app/productos/espumante").then((resp)=>{
+            setEspumante(resp.data)
+        })
+    }
+
 
     return (
 
         <div>
             <div className='img__container'>
-                <img src={vinos} alt="imagen vinos" />
+                <img src={imgVinos} alt="imagen vinos" />
             </div>
             <div className='table__container'>
                 <h2 className='food-title'>Vinos</h2>
@@ -32,13 +42,41 @@ const MenuTable = () => {
                         </tr>
                     </thead>
                     <tbody className='tbody'>
-                        {menuItems.map((item, index) => (
+                        {vinos.map((item, index) => (
                             <tr className='tr' key={index}>
-                                <td className='td'>{item.name}</td>
-                                <td className='td'>${item.price}</td>
+                                <td className='td'>{item.nombre}</td>
+                                <td className='td'>${item.precio}</td>
                             </tr>
                         ))}
                     </tbody>
+                    <thead className='thead'>
+                        <tr className='tr'>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div className='img__container'>
+                <img src={imgEspumante} alt="imagen vinos" />
+            </div>
+            <div className='table__container'>
+                <h2 className='food-title'>Espumante</h2>
+                <table className='table'>
+                    <thead className='thead'>
+                        <tr className='tr'>
+                        </tr>
+                    </thead>
+                    <tbody className='tbody'>
+                        {espumante.map((item, index) => (
+                            <tr className='tr' key={index}>
+                                <td className='td'>{item.nombre}</td>
+                                <td className='td'>${item.precio}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <thead className='thead'>
+                        <tr className='tr'>
+                        </tr>
+                    </thead>
                 </table>
             </div>
         </div>
