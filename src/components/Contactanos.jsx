740679e51218de986c2b6798aa107dfd4c64f73c
3steban99 <barrionuevo99@outlook.com>
2023-08-07 16:28:22 -0300

    fix de contactanos

--- src/components/Contactanos.jsx
+++ src/components/Contactanos.jsx
@@ -3,30 +3,30 @@ import '../styles/Contactanos.scss';
 import { GeoAltFill, EnvelopeFill, TelephoneFill, Instagram, Facebook } from 'react-bootstrap-icons';
 
 const Contactanos = () => {
- return (
-  
-  <div className='container-container' id='#section-social'>
-    <h2 className='home-title'>Contactanos!</h2>
-    <div className='social-container' data-aos="fade-down" data-aos-duration="500">
-     <div className='square-contacto'>
-      <h2 className='contactanos-text'>Beat Club Mendoza</h2>
-      <div>
-       <p className='contactanos-link' id='location-link'>Av.Perú 1773, M5500 Mendoza, Argentina</p>
-       <p className='contactanos-link' id='mail-link'>beatclubmendoza@gmail.com</p>
-       <p className='contactanos-link' id='phone-link'>(261) 341-7030</p>
-       <a href='https://www.instagram.com/beatclubmendoza/' className='contactanos-link' id='instagram-link'>@beatclubmendoza</a>
-       <a href='https://www.facebook.com/beatclubpianobar/' className='contactanos-link' id='facebook-link'>@beatclubmendoza</a>
-      </div>
-      <GeoAltFill className='icon-logo' />
-      <EnvelopeFill className='icon-logo' />
-      <TelephoneFill className='icon-logo' />
-      <Instagram className='icon-logo' id='instagram-logo' />
-      <Facebook className='icon-logo' id='facebook-logo' />
-     </div>
-    </div>
+  return (
 
-  </div>
- )
+    <div className='container-container' id='#section-social'>
+      <h2 className='home-title'>Contactanos!</h2>
+      <div className='social-container' data-aos="fade-down" data-aos-duration="500">
+        <div className='square-contacto'>
+          <h2 className='contactanos-text'>Beat Club Mendoza</h2>
+          <div>
+            <p className='contactanos-link' id='location-link'>Av.Perú 1773, M5500 Mendoza, Argentina</p>
+            <p className='contactanos-link' id='mail-link'>beatclubmendoza@gmail.com</p>
+            <p className='contactanos-link' id='phone-link'>(261) 341-7030</p>
+            <a href='https://www.instagram.com/beatclubmendoza/' className='contactanos-link' id='instagram-link'>@beatclubmendoza</a>
+            <a href='https://www.facebook.com/beatclubpianobar/' className='contactanos-link' id='facebook-link'>@beatclubmendoza</a>
+          </div>
+          <GeoAltFill className='icon-logo' />
+          <EnvelopeFill className='icon-logo' />
+          <TelephoneFill className='icon-logo' />
+          <Instagram className='icon-logo' id='instagram-logo' />
+          <Facebook className='icon-logo' id='facebook-logo' />
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default Contactanos;
