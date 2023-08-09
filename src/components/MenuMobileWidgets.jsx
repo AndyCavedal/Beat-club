b869f38b708bf7ced238444c102ca12da3a4bfd5
3steban99 <barrionuevo99@outlook.com>
2023-08-09 20:50:05 -0300

    testeo de MenuMobile

--- src/components/MenuMobileWidgets.jsx
+++ src/components/MenuMobileWidgets.jsx
@@ -19,43 +19,43 @@ const MenuMobileWidget = () => {
             <h2 className="widgets-title">Nuestra Carta</h2>
             <div className="grid-item">
                 <img src={vinos} alt="seccion vinos" />
-                <a className='menu-btn' href="/">VINOS</a>
+                <a className='menu-btn' href="#vinos">VINOS</a>
             </div>
             <div className="grid-item">
                 <img src={picadas} alt="seccion picadas" />
-                <a className='menu-btn' href="/">PICADAS</a>
+                <a className='menu-btn' href="#picadas">PICADAS</a>
             </div>
             <div className="grid-item">
                 <img src={bebidas} alt="seccion bebidas" />
-                <a className='menu-btn' href="/">BEBIDAS</a>
+                <a className='menu-btn' href="#bebidas">BEBIDAS</a>
             </div>
             <div className="grid-item">
                 <img src={sandwichs} alt="seccion sandwichs" />
-                <a className='menu-btn' href="/">SANDWICHS</a>
+                <a className='menu-btn' href="#sandwiches">SANDWICHS</a>
             </div>
             <div className="grid-item">
                 <img src={burgers} alt="seccion burgers" />
-                <a className='menu-btn' href="/">BURGERS</a>
+                <a className='menu-btn' href="#burgers">BURGERS</a>
             </div>
             <div className="grid-item">
                 <img src={promos} alt="seccion promos" />
-                <a className='menu-btn' href="/">PROMOS</a>
+                <a className='menu-btn' href="#promociones">PROMOS</a>
             </div>
             <div className="grid-item">
                 <img src={postres} alt="seccion postres" />
-                <a className='menu-btn' href="/">POSTRES</a>
+                <a className='menu-btn' href="#postres">POSTRES</a>
             </div>
             <div className="grid-item">
                 <img src={tragos} alt="seccion tragos" />
-                <a className='menu-btn' href="/">TRAGOS</a>
+                <a className='menu-btn' href="#forever young">TRAGOS</a>
             </div>
             <div className="grid-item">
                 <img src={whiskys} alt="seccion whiskys" />
-                <a className='menu-btn' href="/">WHISKYS</a>
+                <a className='menu-btn' href="#speakeasy licor scotch">WHISKYS</a>
             </div>
             <div className="grid-item">
                 <img src={ensaladas} alt="seccion ensaladas" />
-                <a className='menu-btn' href="/">ENSALADAS</a>
+                <a className='menu-btn' href="#ensaladas">ENSALADAS</a>
             </div>
         </div>
     );
