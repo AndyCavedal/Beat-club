b4d83c91e8f9c9a2b6d78c9234e897a6ca47eb20
Andy Cavedal <hcavedal@gmail.com>
2023-08-21 22:57:46 -0300

    [fix] testeo menu scroll

--- src/components/MenuBienvenida.jsx
+++ src/components/MenuBienvenida.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import '../styles/MenuBienvenida.scss';
 
 const MenuBienvenida = () => {
+    const [scrollPosY, setScrollPosY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <div className='menu__container'>
-            <div className='menu-background'>
+            <div className='menu-background' style={{ transform: `translateY(-${scrollPosY}px)` }}>
             <h1 className='menu-title'>Bienvenidos a <br></br> Beat Club Mendoza</h1>
             </div>
         </div>
