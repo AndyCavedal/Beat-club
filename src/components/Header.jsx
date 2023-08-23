4843fa3968fc6916aa55ab5a12307a7e215b5ddc
Andy Cavedal <hcavedal@gmail.com>
2023-08-22 21:01:49 -0300

    [add] logo mobile fixed y enrutado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/beat-club-logo-header.png';
 import { List, X } from 'react-bootstrap-icons';
 import { useRef, useState, useEffect } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import { Link as ScrollLink } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 
 
 const Header = () => {
@@ -34,6 +34,10 @@ const Header = () => {
         setNavVisible(false);
     };
 
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
     return (
         <header>
             <nav ref={navRef} className={isNavVisible ? 'responsive_nav' : ''}>
@@ -58,7 +62,9 @@ const Header = () => {
                 </ul>
             </nav>
             <List onClick={showNavbar} className="nav-btn mobile" />
-            <img src={logo} alt="beat-club logo" href="/" id="logo-header" className="mobile" />
+            <Link to="/" onClick={scrollToTop}>
+                <img src={logo} alt="beat-club logo" href="/" id="logo-header" className="mobile" />
+            </Link>
         </header>
     )
 }
