7d09ee1f8e45abc6949a4462c51999db386a0d40
Andy <hcavedal@gmail.com>
2023-09-11 21:40:39 -0300

    [fix] no translate about title, arreglado entrada mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = ({ name }) =>{
         <div id={name} className='about-panel__container'>
             <div id='about-section' className='about-title__container'>
                 <h2 className='about-title'>Acerca de</h2>
-                <h3 className='about-subtitle'>Beat <span className='club-text'>Club</span></h3>
+                <h3 className='about-subtitle' translate='no'>Beat <span className='club-text'>Club</span></h3>
             </div>
             <div className='about-text__container'>
                 <p className='about-text'>Beat Club es una sala de espectáculos con bar, un lugar cálido con buen ambiente donde se puede disfrutar tanto de una rica cena, variedad de cócteles y buena música en vivo.</p>
--- src/components/Entrada.jsx
+++ src/components/Entrada.jsx
@@ -7,8 +7,8 @@ const Entrada = () => {
         <div className='container__container' data-aos="fade-down" data-aos-duration="500">
             <div className='entradaweb__container'>
                 <div className='entradaweb__container-izq'>
-                    <h2 className='entrada-text' id='entrada-title'>Compra tu entrada ahora mismo!</h2>
-                    <h2 className='entrada-text' id='entrada-subtitle'>Compra tu entrada y veni a vivir una experiencia única en Beat Club.</h2>
+                    <h2 className='entrada-text' id='entrada-title'>Compra tu Entrada ahora mismo!</h2>
+                    <h2 className='entrada-text' id='entrada-subtitle'>Vení a vivir una experiencia única en Beat Club.</h2>
                     <a href='https://www.entradaweb.com.ar/index.php?r=site%2Flistado-filtrado&recinto=RCTO-24e15b33' target='_blank' className='btn'>COMPRA TU ENTRADA</a>
                 </div>
                 <img src={publico} alt="Foto de espectáculo" className='entradaweb-img' loading="lazy" />
